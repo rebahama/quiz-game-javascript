5200aef39d4200b9fd1694c81b559d229c7cc6fe
rebahama <wiri@hotmail.se>
2022-03-10 05:10:12 +0000

    style the radio buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,37 +1,50 @@
 //variabels to acess the id in the html file.
-let showQuiz=document.getElementById('quiz-form');
-let showPlay=document.getElementById('play-btn');
-let showFlex=document.getElementById('flex-container-1');
-
-
+let  showQuiz=document.getElementById('quiz-form');
+let  showPlay=document.getElementById('play-btn');
+let  showFlex=document.getElementById('flex-container-1');
+const  answerArray=["Barack Obama", "4"] 
 // display the id when clicking the variabe and hideing the div.
 // if statment for when the field is empty 
 showPlay.addEventListener('click', function showArea() {
-    let emptyField=document.getElementById('input-name').value
-    if(emptyField=="")
-    {
-        alert('Please write a username before clicking play')
-        
-        return false
 
-    }
-    else if(emptyField){
+
+    let  emptyField=document.getElementById('input-name').value
+    if  (emptyField==""){
+        alert('Please write a username before clicking play')
+        return false}
+    else if (emptyField){
         showQuiz.style.display="block";
         showFlex.style.display="none";
-
     }
-    
-    
-
-
 
 })
-// when clicking the showplay variale the reciveInput variable takes value from the input-name and injects it into the user-name id with a click.
-showPlay.addEventListener('click', function reciveUsername(){
-    let reciveInput=document.getElementById('input-name').value
-document.getElementById('user-name').innerHTML=reciveInput;
+// when clicking the showplay variable and reciveInput variable takes value from the input-name and injects it into the user-name id with a click.
+showPlay.addEventListener('click', function reciveUsername() {
+
+
+  let reciveInput=document.getElementById('input-name').value
+  document.getElementById('user-name').innerHTML=reciveInput;
 
 })
+
+// function for recieving the values from html radio button and checking if the answers are correct with the array index. If it is correct then it will increment.
+function answerScore(){
+
+
+    let question1=document.quiz.question1.value;
+    let question2=document.quiz.question2.value;
+    let currentScore=0
+
+    if(question1===answerArray[0]){
+        currentScore++;
+    }
+    if(question2===answerArray[1]){
+        currentScore++;
+    }
+    console.log(currentScore)
+}
+
+
 
 
 
