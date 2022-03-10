5e35f0c75a75494533a34ffcb271a661053bde3a
rebahama <wiri@hotmail.se>
2022-03-10 02:49:16 +0000

    add if statement for empty field

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,10 +5,26 @@ let showFlex=document.getElementById('flex-container-1');
 
 
 // display the id when clicking the variabe and hideing the div.
+// if statment for when the field is empty 
 showPlay.addEventListener('click', function showArea() {
+    let emptyField=document.getElementById('input-name').value
+    if(emptyField=="")
+    {
+        alert('Please write a username before clicking play')
+        
+        return false
 
-showQuiz.style.display="block";
-showFlex.style.display="none";
+    }
+    else if(emptyField){
+        showQuiz.style.display="block";
+        showFlex.style.display="none";
+
+    }
+    
+    
+
+
+
 })
 // when clicking the showplay variale the reciveInput variable takes value from the input-name and injects it into the user-name id with a click.
 showPlay.addEventListener('click', function reciveUsername(){
@@ -16,4 +32,6 @@ showPlay.addEventListener('click', function reciveUsername(){
 document.getElementById('user-name').innerHTML=reciveInput;
 
 })
+
+
 
