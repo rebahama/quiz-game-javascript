6d29d062ef246c9c31dba64b7e00e5e41ca12171
rebahama <wiri@hotmail.se>
2022-03-10 02:21:54 +0000

    add functionality for username

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,11 +4,16 @@ let showPlay=document.getElementById('play-btn');
 let showFlex=document.getElementById('flex-container-1');
 
 
-// display the id when clicking the variabe and hideing the div
+// display the id when clicking the variabe and hideing the div.
 showPlay.addEventListener('click', function showArea() {
 
 showQuiz.style.display="block";
-showFlex.style.display="none"
+showFlex.style.display="none";
+})
+// when clicking the showplay variale the reciveInput variable takes value from the input-name and injects it into the user-name id with a click.
+showPlay.addEventListener('click', function reciveUsername(){
+    let reciveInput=document.getElementById('input-name').value
+document.getElementById('user-name').innerHTML=reciveInput;
 
 })
 
