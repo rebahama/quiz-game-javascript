5b646a5a8a8383406ae650c5ea5f521fabb0c695
rebahama <wiri@hotmail.se>
2022-03-13 18:29:44 +0000

    add metatags and missing semicolon

---
+++ README.md
@@ -0,0 +1,9 @@
+# QuizIt
+Quizit is a very simple and basic question and answer application. As soon you get to the landingpage a short description will be presented and it will be clear what the page is about. The target audience is basically anyone who is up for a few seconds of a challange. The outcome after visiting the site is that you will maybe have learned a few things that you didn´t know before. It´s a fun way of makeing learning easy and avalible for anyone available.
+# User stories
+Sometimes As I browse the internet as a user I would like to learn something random in a fun and easy way, the topic is not important it could be anything as long I learn something new.
+
+# Features
+* Existing features
+
+
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,4 @@
+
 //variabels to access the id in the html file.
 let showQuiz = document.getElementById('quiz-form');
 let showPlay = document.getElementById('play-btn');
@@ -15,41 +16,42 @@ let showAllAnswers = document.getElementById('show-all-answers-btn');
 let introHeading = document.getElementById('intro-heading');
 let restartButton = document.getElementById('restart-btn');
 let wrongInput = document.getElementById('wrong-input-text');
-let currentScore = 0
+let currentScore = 0;
 
 //Array for holding the answer values
 const answerArray = ['Barack Obama', '15', 'Russia', 'Caspian Sea', '1914', 'Elon Musk', '1945', '29', 'China'];
 const wrongArray = ['Donald Trump', '36', 'China', 'Baikal Lake', '1919', 'Jeff Bezos', '1943', '45', 'India'];
 // Array for holding the diffrent div elements in html document
-const allSection = [firstArea, secondArea, thirdArea]
+const allSection = [firstArea, secondArea, thirdArea];
 
 nextButton.addEventListener('click', function nextSection() {
     for (let i = 0; i < allSection.length; i++) {
 
         if (allSection[0].style.display = 'block') {
-            allSection[1].style.display = 'block'
-            allSection[0].style.display = 'none'
+            allSection[1].style.display = 'block';
+            allSection[0].style.display = 'none';
 
 
         }
 
-        allSection[2].style.display = "block"
-        allSection[1].style.display = "block"
+        allSection[2].style.display = "block";
+        allSection[1].style.display = "block";
 
     }
 
 
-})
+});
+answerScore();
 
 // display the id when clicking the variabe and hideing the div.
 // if statment for when the field is empty 
 showPlay.addEventListener('click', function showArea() {
-let noInputText="Please write a username before clicking start"
-    let emptyField = document.getElementById('input-name').value
+let noInputText="Please write a username before clicking start";
+    let emptyField = document.getElementById('input-name').value;
     if (emptyField == "") {
         wrongInput.innerHTML=noInputText;
-        alert('Please write a username before clicking play')
-        return false
+        alert('Please write a username before clicking play');
+        return false;
     } else if (emptyField) {
 
         showQuiz.style.display = "block";
@@ -62,13 +64,13 @@ let noInputText="Please write a username before clicking start"
 // when clicking the showplay variable and reciveInput variable takes value from the input-name and injects it into the user-name id with a click.
 showPlay.addEventListener('click', function reciveUsername() {
 
-    let reciveInput = document.getElementById('input-name').value
+    let reciveInput = document.getElementById('input-name').value;
     userName.innerHTML = reciveInput;
 
-})
+});
 // function for recieving the values from html radio button and checking if the answers are correct with the array index. If it is correct then it will increment.
 
-function answerScore() {
+function answerScore(){
 
     let question1 = document.quiz.question1.value;
     let question2 = document.quiz.question2.value;
@@ -156,21 +158,21 @@ showAllAnswers.addEventListener('click', function showEverything() {
     showQuiz.style.display = "block";
 
 
-})
+});
 // click event and function showing how many points you got from the quiz based on your clicks.
 submitButton.addEventListener('click', function showSubmitArea() {
 
 
     showQuiz.style.display = "none";
     submitArea.style.display = "block";
-    let showallscore = `Good job ${userName.innerHTML} you answered right on ${currentScore} questions out of 9. Click show quiz to view how you answered`
-    console.log(showallscore)
+    let showallscore = `Good job ${userName.innerHTML} you answered right on ${currentScore} questions out of 9. Click show quiz to view how you answered`;
+    console.log(showallscore);
     document.getElementById('answer-score').innerHTML = showallscore;
 
 
-})
+});
 //Restart game clickevent
 restartButton.addEventListener('click', function restartGame() {
     restartButton = location.reload();
 
-})
+});
