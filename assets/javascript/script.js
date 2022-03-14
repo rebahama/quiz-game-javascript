64fd7057144dbb18576617e38b6e35d53e154a0c
rebahama <wiri@hotmail.se>
2022-03-14 22:47:37 +0000

    add missing semicolon

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,14 +10,9 @@ let thirdArea = document.getElementById('next-area-3');
 let submitButton = document.getElementById('submit-btn');
 let radioBackground = document.getElementsByClassName('radio-container');
 let currentSection = firstArea;
-
 let userName = document.getElementById('answer-score');
 let showAllAnswers = document.getElementById('show-all-answers-btn');
-
 let restartButton = document.getElementById('restart-btn');
-
-
-
 let currentScore = 0;
 //Array for holding the answer values
 const answerArray = ['Barack Obama', '15', 'Russia', 'Caspian Sea', '1914', 'Elon Musk', '1945', '29', 'China'];
@@ -25,14 +20,14 @@ const wrongArray = ['Donald Trump', '36', 'China', 'Baikal Lake', '1919', 'Jeff 
 // Array for holding the diffrent div elements in html document
 const allSection = [firstArea, secondArea, thirdArea];
 //array for holding images
-const imageArray = ['assets/images/gold-medal.jpeg', 'assets/images/silver-medal.jpeg', 'assets/images/sad-smiley.jpeg']
+const imageArray = ['assets/images/gold-medal.jpeg', 'assets/images/silver-medal.jpeg', 'assets/images/sad-smiley.jpeg'];
 
 
+/**
+ * The next and previous button that shows diffrent section divs in the same html form
+ */
 
-
-
-nextButton.addEventListener('click', nextSection)
-
+nextButton.addEventListener('click', nextSection);
 
 function nextSection() {
     if (currentSection === firstArea) {
@@ -47,22 +42,20 @@ function nextSection() {
         console.log("Invalid case");
     }
 
-};
+}
 
-previousButton.addEventListener('click', previousBack)
+previousButton.addEventListener('click', previousBack);
 
 function previousBack() {
 
     if (currentSection === thirdArea) {
         thirdArea.style.display = "none";
         secondArea.style.display = "block";
-        currentSection = secondArea
-    }
-    
-   else if (currentSection === secondArea) {
-        secondArea.style.display = "none"
-        firstArea.style.display = "block"
-        currentSection = firstArea
+        currentSection = secondArea;
+    } else if (currentSection === secondArea) {
+        secondArea.style.display = "none";
+        firstArea.style.display = "block";
+        currentSection = firstArea;
     }
 
 }
@@ -70,9 +63,10 @@ function previousBack() {
 
 answerScore();
 
-// display the id when clicking the variabe and hideing the div.
-// if statment for when the field is empty 
-showPlay.addEventListener('click', showArea)
+/**
+ * Prevents the game from starting if the user leaves the input field empty.
+ */
+showPlay.addEventListener('click', showArea);
 
 
 function showArea() {
@@ -91,17 +85,22 @@ function showArea() {
     }
     introHeading.style.display = "none";
 
-};
-// when clicking the showplay variable and reciveInput variable takes value from the input-name and injects it into the user-name id with a click.
-showPlay.addEventListener('click', receiveUserName)
+}
+
+/**
+ * Puts the value from the field the user writes in and puts that value to a element in html affter clicking play button
+ */
+showPlay.addEventListener('click', receiveUserName);
 
 function receiveUserName() {
 
     let reciveInput = document.getElementById('input-name').value;
     userName.innerHTML = reciveInput;
 
-};
-// function for recieving the values from html radio button and checking if the answers are correct with the array index. If it is correct then it will increment.
+}
+/**
+ *  Takes value from radibutton in html and compares it to an array and increments score if the score is right.
+ */
 
 function answerScore() {
 
@@ -179,8 +178,13 @@ function answerScore() {
     }
 
 }
-// After submiting quiz click on show answers to see what the user got right and wrong and also hide all the buttons.
-showAllAnswers.addEventListener('click', function showEverything() {
+
+/**
+ *  After clicking submit button hides the button inside the quizform
+ */
+showAllAnswers.addEventListener('click', showEverything);
+
+function showEverything() {
     for (let i = 0; i < allSection.length; i++) {
 
         allSection[i].style.display = "block";
@@ -192,13 +196,16 @@ showAllAnswers.addEventListener('click', function showEverything() {
     showQuiz.style.display = "block";
 
 
-});
-// click event and function showing how many points you got from the quiz based on your clicks and not displaying the intro after clicking submit
-submitButton.addEventListener('click', showSubmitArea)
+}
+
+/**
+ * After clicking submit button hideing header and intro rules and displaying a message with your name and score. Also displaying an image depanding on how many right answers you get.
+ */
+submitButton.addEventListener('click', showSubmitArea);
 
 function showSubmitArea() {
     let introHeading = document.getElementById('intro-heading');
-    let introRules = document.getElementById('intro-rules')
+    let introRules = document.getElementById('intro-rules');
     let submitArea = document.getElementById('submit-area');
     introRules.style.display = "none";
     introHeading.style.display = "none";
@@ -208,20 +215,24 @@ function showSubmitArea() {
     console.log(showallscore);
     document.getElementById('answer-score').innerHTML = showallscore;
     // if statements that displays images in array based on score from increment
-    let imageScore = document.getElementById('image-score')
+    let imageScore = document.getElementById('image-score');
     if (currentScore < 3) {
-        imageScore.src = imageArray[2]
+        imageScore.src = imageArray[2];
     } else if (currentScore >= 5) {
-        imageScore.src = imageArray[1]
+        imageScore.src = imageArray[1];
     }
     if (currentScore >= 6) {
-        imageScore.src = imageArray[0]
+        imageScore.src = imageArray[0];
     }
+
+    console.log(window.onload)
+    window.onload = showSubmitArea();
 };
 
-
-//Restart game clickevent
-restartButton.addEventListener('click', restartGame)
+/**
+ * After clicking the reset button the quiz runs from the beginning
+ */
+restartButton.addEventListener('click', restartGame);
 
 function restartGame() {
     restartButton = location.reload();
